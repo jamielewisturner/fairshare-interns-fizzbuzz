f29b9611e9aa3477e47501eced1f3a2b752ec8f2
Jamie Turner (Local Admin) <JamTurAdmin@softwire.com>
2023-07-03 16:12:03 +0100

    Append Bang if multiple of 7

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,29 @@
 // This is our main function
 function fizzbuzz() {
 
+
+   
+
     for (var i = 1; i <= 100; i++){
-        if (i%3 ==  0 && i%5 ==  0){
-            console.log("FizzBuzz")
-        }else if(i%3 == 0){
-            console.log("Fizz")
-        }else if(i%5 == 0){
-            console.log("Buzz")
-        }else{
-            console.log(i)
+
+        var output = "";
+        if (i%3 == 0){
+            output+="Fizz";
         }
+        if (i%5 == 0){
+            output+="Buzz";
+        }
+        if (i%7 == 0){
+            output+="Bang";
+        }
+        
+
+        if (output == ""){
+            output = i;
+        }
+        console.log(output);
     }
+
 
 }
 
