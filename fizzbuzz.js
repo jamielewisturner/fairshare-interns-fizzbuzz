fa4002121d2c6f5e0a48fc15b9c3e71c2c1d933d
Jamie Turner (Local Admin) <JamTurAdmin@softwire.com>
2023-07-03 17:22:53 +0100

    Added test suite

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,41 +1,45 @@
 //import {argv} from "node:process"
 const prompt = require("prompt-sync")({sigint : true})
 // This is our main function
-function fizzbuzz(N) {
+
+function fizzbuzz(i, options){
+    var output = [];
+    if (i%3 == 0 && options[3]){
+        output.push("Fizz");
+    }
+    if (i%5 == 0 && options[5]){
+        output.push("Buzz");
+    }
+    if (i%7 == 0 && options[7]){
+        output.push("Bang");
+    }
+    if (i%11 == 0 && options[11]){
+        output = ["Bong"];
+    }
+    if (i%13 == 0 && options[13]){
+        var j = 0;
+        for ( ; j < output.length; j++){
+            if (output[j][0] == "B"){
+                break;
+            }
+        }
+        output.splice(j, 0, "Fezz")
+    }
+    if (i%17 == 0 && options[17]){
+        output = output.reverse()
+    }
+    
+
+    if (output.length == 0){
+        output = [i];
+    }
+    return output.join("");
+}
+
+function fizzbuzzAll(N, options) {
     
     for (var i = 1; i <= N; i++){
-
-        var output = [];
-        if (i%3 == 0 && options[3]){
-            output.push("Fizz");
-        }
-        if (i%5 == 0 && options[5]){
-            output.push("Buzz");
-        }
-        if (i%7 == 0 && options[7]){
-            output.push("Bang");
-        }
-        if (i%11 == 0 && options[11]){
-            output = ["Bong"];
-        }
-        if (i%13 == 0 && options[13]){
-            var j = 0;
-            for ( ; j < output.length; j++){
-                if (output[j][0] == "B"){
-                    break;
-                }
-            }
-            output.splice(j, 0, "Fezz")
-        }
-        if (i%17 == 0 && options[17]){
-            output = output.reverse()
-        }
-        
-
-        if (output.length == 0){
-            output = [i];
-        }
-        console.log(output.join(""));
+        fizzbuzz(i, options)
     }
 
 }
@@ -50,7 +54,8 @@ process.argv.forEach((val, index) => {
 
 
 // Now, we run the main function:
-const N = prompt("Enter number> ");
-fizzbuzz(N);
+//const N = prompt("Enter number> ");
+//fizzbuzzAll(N, options);
 
 
+module.exports = fizzbuzz
